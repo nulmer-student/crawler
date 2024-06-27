f313f3ede9d4e124fef42e1f7b311696ad7862ed
nulmer-student <nulmer@ualberta.ca>
2024-06-27 16:27:11 -0600

    Save work

--- miner/src/Compile.cpp
+++ miner/src/Compile.cpp
@@ -5,6 +5,8 @@
 #include <format>
 #include <iostream>
 
+#define CLANG_PATH "/home/nju/.opt/scalar/llvm-bin/bin/clang"
+
 namespace Miner {
 
 void compile_all(DepGraph dg) {
@@ -20,48 +22,19 @@ void compile_all(DepGraph dg) {
         node_vec.push_back(*i);
     }
 
-    // Compile each file
     #pragma omp parallel for
     for (int i = 0; i < node_vec.size(); i++) {
-        string output = "";
-
-        Key key = node_vec[i].first;
-        Node file = node_vec[i].second;
-
-        if (file.path.string().back() != 'c')
-            continue;
-
-        // Find dependencies
-        output += format("Processing file: {}\n", file.path.string());
+        #pragma omp atomic
         file_count += 1;
-        KeySet *deps = new KeySet{};
-        dg.naive_deps(key, Include("<>"), deps);
-
-        // Get the include directories
-        Keys dirs = dg.find_dirs(deps);
-
-        // Format includes
-        string includes = "";
-        for (auto ii = dirs.begin(); ii != dirs.end(); ii++) {
-            includes += "-I" + ii->string() + " ";
-        }
 
         // Compile the file
-        string command = format(
-            "clang -c {} {} -emit-llvm -o - 2> /dev/null",
-            file.path.string(),
-            includes);
+        Compiler c = Compiler(&dg);
+        CompileResult result = c.compile_one(node_vec[i].second);
 
-        auto result = run_process(command);
-        if (result.second != 0) {
+        // Update statistics
+        if (!result.success)
+            #pragma omp atomic
             error_count += 1;
-            output += format("failed\n");
-        } else {
-            output += format("success\n");
-        }
-
-        delete deps;
-        cout << output;
     }
 
     // Print statistics
@@ -69,6 +42,53 @@ void compile_all(DepGraph dg) {
     cout << format("Total files: {:5}\n", file_count);
     cout << format("Successful:  {:5} ({:5.1f}%)\n", file_count - error_count, 100.0 - prop);
     cout << format("Errors:      {:5} ({:5.1f}%)\n", error_count, prop);
+}
+
+// =============================================================================
+// Compiler
+// =============================================================================
+
+CompileResult Compiler::compile_one(Node file) {
+    string output = "";
+
+    // Find dependencies
+    output += format("Processing file: {}\n", file.path.string());
+    KeySet *deps = new KeySet{};
+    dg->naive_deps(file.path, Include("<>"), deps);
+
+    // Get the include directories
+    Keys dirs = dg->find_dirs(deps);
+
+    // Format includes
+    string includes = "";
+    for (auto ii = dirs.begin(); ii != dirs.end(); ii++) {
+        includes += "-I" + ii->string() + " ";
+    }
+
+    // Compile the file
+    string command = format(
+        "{} -c {} {} -o /dev/null -emit-llvm",
+        // "{} -c {} {} -o /dev/null -emit-llvm -O3 -Rpass=loop-vectorize",
+        // CLANG_PATH,
+        "clang",
+        file.path.string(),
+        includes);
+
+    auto result = run_process(command);
+    output += result.first;
+    auto success = true;
+
+    if (result.second != 0) {
+        output += format("failed\n");
+        success = false;
+    } else {
+        output += format("success\n");
+    }
+
+    delete deps;
+    cout << output;
+
+    return CompileResult(success);
 }
 
 } // namespace Miner
--- miner/src/Compile.h
+++ miner/src/Compile.h
@@ -6,6 +6,21 @@
 using namespace std;
 namespace Miner {
 
+struct CompileResult {
+    bool success;
+};
+
+// Compiles a single file, checking all possible header choices
+class Compiler {
+public:
+    Compiler(DepGraph *dg) : dg(dg){};
+
+    CompileResult compile_one(Node file);
+
+private:
+    DepGraph *dg;
+};
+
 void compile_all(DepGraph dg);
 
 }
