86f582d914ffba882923d604cd77d8caca477acf
nulmer-student <nulmer@ualberta.ca>
2024-06-27 16:27:11 -0600

    Print stacktrace on crash

--- miner/src/Main.cpp
+++ miner/src/Main.cpp
@@ -2,12 +2,31 @@
 #include "Deps.h"
 #include "Compile.h"
 
+#include <csignal>
+#include <cstdio>
 #include <string>
+#include <execinfo.h>
+#include <signal.h>
+#include <unistd.h>
 
 using namespace Miner;
 using namespace std;
 
+void handler(int sig) {
+    // Get stackframes
+    void *array[10];
+    int size = backtrace(array, 10);
+
+    // Print out stackframes
+    fprintf(stderr, "Error: singal %d\n", sig);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
+    exit(1);
+}
+
 int main(int argc, char *argv[]) {
+    // Register signal handler
+    signal(SIGSEGV, handler);
+
     // Find all source files in the repository
     string path(argv[1]);
     vector<filesystem::path> cc      = find_files(path, "c");
