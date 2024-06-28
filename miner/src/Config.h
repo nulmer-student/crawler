39d8ed506f2c79c17d1fdc68fb5caf5f194729ac
nulmer-student <nulmer@ualberta.ca>
2024-06-28 13:45:36 -0600

    Add argument parsing

---
+++ miner/src/Config.h
@@ -0,0 +1,65 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#include <argp.h>
+#include <bits/types/error_t.h>
+#include <string>
+#include <filesystem>
+
+// Number of required arguments
+#define N_REQUIRED 1
+
+using namespace std;
+
+namespace Miner {
+
+const char *argp_program_version = "0.1.0";
+static char doc[] = "TODO: Add docs";
+
+// Required options
+static char args_doc[] = "REPO";
+
+// Switch options
+static struct argp_option options[] = {
+    {"threads", 't',    "N", 0, "Number of threads to use"},
+    {"log",     'l', "FILE", 0, "Path to the log"},
+    { 0 }
+};
+
+struct arguments {
+    char *args[N_REQUIRED];
+    int threads;
+    filesystem::path log;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct arguments *arguments = static_cast<struct arguments*>(state->input);
+
+    switch (key) {
+        case 't':
+            arguments->threads = stoi(arg);
+            break;
+        case 'l':
+            arguments->log = filesystem::path(arg);
+            break;
+        case ARGP_KEY_ARG:
+            if (state->arg_num >= N_REQUIRED)
+                argp_usage(state);
+            arguments->args[state->arg_num] = arg;
+            break;
+        case ARGP_KEY_END:
+            if (state->arg_num < N_REQUIRED)
+                argp_usage(state);
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+};
+
+static struct argp arg_p = { options, parse_opt, args_doc, doc };
+
+}
+
+#endif // CONFIG_H_
--- miner/src/Main.cpp
+++ miner/src/Main.cpp
@@ -1,6 +1,7 @@
 #include "Util.h"
 #include "Deps.h"
 #include "Compile.h"
+#include "Config.h"
 
 #include <csignal>
 #include <cstdio>
@@ -8,6 +9,7 @@
 #include <execinfo.h>
 #include <signal.h>
 #include <unistd.h>
+#include <omp.h>
 
 using namespace Miner;
 using namespace std;
@@ -23,12 +25,22 @@ void handler(int sig) {
     exit(1);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
     // Register signal handler
     signal(SIGSEGV, handler);
 
+    // Handle arguments
+    struct arguments args;
+    args.threads = 12;
+    args.log = "./log";
+
+    argp_parse(&arg_p, argc, argv, 0, 0, &args);
+
+    // Set the number of threads to use
+    omp_set_num_threads(args.threads);
+
     // Find all source files in the repository
-    string path(argv[1]);
+    string path(args.args[0]);
     vector<filesystem::path> cc      = find_files(path, "c");
     vector<filesystem::path> headers = find_files(path, "h");
 
