f3f9194f76ab5052a8ecae0943f2782c55bb644c
nulmer-student <nulmer@ualberta.ca>
2024-07-02 14:59:10 -0600

    Add interning

--- runner/database.py
+++ runner/database.py
@@ -21,4 +21,73 @@ class Database:
 
 
 class InternDB(Database):
-    pass
+    def _init_db(self):
+        super()._init_db()
+
+        # Add tables
+        self.cursor.execute("drop table if exists files")
+        self.cursor.execute("drop table if exists matches")
+
+        self.cursor.execute(
+            """
+            create table files (
+                file_id     int,
+                path        text,
+                primary key (file_id)
+            )
+            """
+        )
+
+        self.cursor.execute(
+            """
+            create table matches (
+                file_id     int,
+                line        int,
+                col         int,
+                vector      int,
+                width       int,
+                si          int,
+                primary key (file_id, line, col)
+            )
+            """
+        )
+
+    def add_match(self, path, line, col, vector, tile, si):
+        # Ensure that there is an entry in the file table
+        id = self._ensure_file(path)
+
+        # Insert the match
+        self.cursor.execute(
+            "insert into matches values (?, ?, ?, ?, ?, ?)",
+            (id, line, col, vector, tile, si)
+        )
+
+        self.connection.commit()
+
+    def _ensure_file(self, path):
+        "Ensure that file PATH exists, & return it's id."
+        # If the file exists, return its id
+        self.cursor.execute(
+            "select file_id from files where path = ?",
+            (path,)
+        )
+
+        if self.cursor.rowcount == 1:
+            return self.cursor.fetchone()
+
+        # Otherwise, insert the file
+        if self.cursor.rowcount == 0:
+            id = self._new_file_id()[0]
+            self.cursor.execute(
+                "insert into files values (?, ?)",
+                (id, path)
+            )
+            self.connection.commit()
+            return id
+
+    def _new_file_id(self):
+        """Gererate a unique file id."""
+        self.cursor.execute("select ifnull(max(file_id) + 1, 0) from files")
+        id = self.cursor.fetchone()
+        self.connection.commit()
+        return id
--- runner/miner.py
+++ runner/miner.py
@@ -6,6 +6,7 @@ import git
 from pathlib import Path
 import os
 import subprocess
+import re
 
 # Setup logging
 import logging
@@ -33,6 +34,9 @@ class Miner:
                 logger.error(f"Failed to find intern script: '{self.intern}'")
                 exit(1)
 
+        # Connect to the database
+        self.db = InternDB()
+
     def _mine_one(self, url, name):
         repo = self._clone(url, name)
         self._mine(repo)
@@ -50,11 +54,23 @@ class Miner:
         if self.intern is None:
             self._intern(output)
         else:
-            pass
+            self._run_intern_script(output)
+
+    def _run_intern_script(self, output):
+        pass
 
     def _intern(self, output):
         """Store the results of running the miner into the database."""
-        print(output)
+        pattern = r"([^,]+),(\d+),(\d+),(\d+),(\d+),(\d+)\n"
+        for match in re.finditer(pattern, output.decode("utf-8")):
+            assert(len(match.groups()) == 6)
+            path   = match.group(1)
+            line   = match.group(2)
+            col    = match.group(3)
+            vector = match.group(4)
+            tile   = match.group(5)
+            si     = match.group(6)
+            self.db.add_match(path, line, col, vector, tile, si)
 
     def _clone(self, url, name):
         """Clone a repository to REPO_DIR."""
