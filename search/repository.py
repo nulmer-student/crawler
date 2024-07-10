50f22c46f2e4caaa863978689f0d43f3082476cf
nulmer-student <nulmer@ualberta.ca>
2024-07-10 16:18:15 -0600

    Remove extra code

--- search/repository.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-class Repo:
-    def __init__(self, id, name, url, stars) -> None:
-        self.id        = int(id)
-        self.name      = name
-        self.clone_url = url
-        self.stars     = int(stars)
