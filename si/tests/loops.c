ef5d6620a108f8bebf486b9f252616f54f689c64
nulmer-student <nulmer@ualberta.ca>
2024-08-13 15:22:50 -0600

    Update test

--- si/tests/loops.c
+++ si/tests/loops.c
@@ -1,33 +1,33 @@
 int many_loops(int a, int b) {
-	for (int i; i < 10; i++) {
-		a += i;
-	}
+    for (int i; i < 10; i++) {
+        a += i;
+    }
 
-	for (int i; i < 10; i++) {
-		for (int j; j < 10; j++) {
-			b += j * i;
-		}
-	}
+    for (int i; i < 10; i++) {
+        for (int j; j < 10; j++) {
+            b += j * i;
+        }
+    }
 
-	for (int i; i < 10; i++) {
-		for (int j; j < 10; j++) {
-			b += j * i;
-		}
+    for (int i; i < 10; i++) {
+        for (int j; j < 10; j++) {
+            b += j * i;
+        }
 
-		for (int j; j < 10; j++) {
-			a += j * i;
-		}
-	}
+        for (int j; j < 10; j++) {
+            a += j * i;
+        }
+    }
 
-	for (int i; i < 10; i++) {
-		for (int j; j < 10; j++) {
-			for (int k; k < 10; k++) {
-				a += j * i + k;
-			}
-		}
-	}
+    for (int i; i < 10; i++) {
+        for (int j; j < 10; j++) {
+            for (int k; k < 10; k++) {
+                a += j * i + k;
+            }
+        }
+    }
 
-	return a + b;
+    return a + b;
 }
 
 
